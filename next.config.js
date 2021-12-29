3fe6c856fa9b74c11231d840b07554c4a43f2b34
ESR <82104698+esrdapp@users.noreply.github.com>
2021-12-29 15:55:14 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,29 +3,9 @@ const withCSS = require("@zeit/next-css");
 const commonsChunkConfig = require("@zeit/next-css/commons-chunk-config");
 const withProgressBar = require('next-progressbar');
 
-
-module.exports = withProgressBar(
-
-withCSS(withSASS({
-productionBrowserSourceMaps: true,
-webpack: (config, { isServer }) => {
-if (!isServer) {
-config = commonsChunkConfig(config, /\.(sass|scss|css)$/)
+module.exports = {
+  generateBuildId: async () => {
+    // You can, for example, get the latest git commit hash here
+    return 'my-build-id'
+  },
 }
-
-return config
-},
-sassLoaderOptions: {
-indentedSyntax: true,
-indentWidth: 4,
-outputStyle: "expanded"
-}
-})
-
-)
-)
-
-
-
-
-;
