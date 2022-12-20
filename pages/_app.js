fa0446b67245910f1eb2c8ac75bff6e48dfdc020
ESR <82104698+esrdapp@users.noreply.github.com>
2022-12-20 01:25:34 +0000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import styles from '../sass/style.module.scss';
+import './sass/style.scss'
 import NextApp, {Container} from "next/app";
 import React from "react";
 import withReduxStore from "../components/hocs/withReduxStore";
